14a5004d8d73e4e07edf00a7b3d6e5b81a77fd8d
panghyuk <okh9712@gmail.com>
2021-10-11 15:00:00 +0900

    마이페이지 1차 수정

--- users/views.py
+++ users/views.py
@@ -1,7 +1,12 @@
 from django.shortcuts import render
+from board.models import Board
 from django.contrib.auth.models import User
 # Create your views here.
 
-def mypage(request):
+def mypage(request,id):
     user = request.user
-    return render(request, 'users/mypage.html')
+    context = {
+        'user':user,
+        'boards':Board.objects.filter(writer=user).order_by('-date'),
+    }
+    return render(request, 'users/mypage.html',context)
